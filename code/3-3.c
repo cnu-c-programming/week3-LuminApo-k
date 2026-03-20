adf93b9b108345a00d167379a2929f51ca2984cc
LuminApo-k <beomseomoon@gmail.com>
2026-03-20 15:38:31 +0900

    Update 3-3.c
    Implement a variadic function to calculate the average of given numbers.

---
+++ code/3-3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+int average(int count, ...) {
+    va_list ap;
+    va_start(ap, count);
+
+    int sum = 0;
+    for (int i = 0; i < count; i++)
+    {
+        sum += va_arg(ap,int);        
+    }    
+
+    int result = sum / count;
+
+    va_end(ap);
+    return result;
+}
+
+int main()
+{
+    printf("%d\n", average(3, 10, 20, 30));
+    printf("%d\n", average(5, 10, 20, 30, 40, 50));
+
+    return 0;
+}
